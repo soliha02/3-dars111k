bb81ede583eb7e0ab43108f668f9c08fe9252b66
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-05-30 19:54:13 +0500

    Add files via upload

---
+++ oq.js
@@ -0,0 +1,14 @@
+let ism = prompt("Sizda hozir random raqamlar chiqadi 🎆");
+let familiya = prompt("Random orqali chiqqan son = 22🎇");
+let yosh = prompt("Biz random sonni 2 ga kopaytirmoqchiqimiz va bolmoqchiqiz ✨");
+let qaysi = prompt("Random orqali chiqqan son = 22 . Biz uni  2 ga kopaytirib 44 sonni chiqardik . Va shuni 2 ga bolib= 11 sonni chiqardik.Va qoldigi bilan 1 sonini chiqardik🧧");
+
+alert(ism,familiya,yosh,qaysi)
+
+
+let omg = prompt("Sizda hozir random raqamlar chiqadi 🎈");
+let oomg = prompt("Random orqali chiqqan son = 33🎄");
+let ooomg = prompt("Biz random sonni 2 ga kopaytirmoqchiqimiz va bolmoqchiqiz 🎃");
+let oooomg = prompt("Random orqali chiqqan son = 33 . Biz uni  2 ga kopaytirib 66 sonni chiqardik . Va shuni 2 ga bolib= 33 sonni chiqardik.Va qoldigi bilan 3 sonini chiqardik🎏");
+
+alert(omg,oomg,ooomg,oooomg)
